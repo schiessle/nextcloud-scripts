5f21858340fb2b09f88d9967dfb4caf9e6a3debc
Bjoern Schiessle <bjoern@schiessle.org>
2016-06-15 14:13:23 +0200

    allow to initialize repsoitories or just udapte them

--- copyLabels.py
+++ copyLabels.py
@@ -1,16 +1,56 @@
 import ConfigParser
 import sys
-
+import getopt
 from lib.LabelManager import LabelManager
 
-if len(sys.argv) != 2:
-    print 'This program copies all labels from the server repository to another repository, defined as argument'
-    print 'Usage:', sys.argv[0], '<repository>'
-    sys.exit(0)
-else:
-    repo = sys.argv[1]
+
+def print_help():
+    print
+    print 'This program copies all labels from the server repository to another repository, defined as argument.'
+    print 'Existig Labels get updated to the values given my the server repository.'
+    print
+    print 'Usage:', sys.argv[0], '-r <repository>'
+    print
+    print 'Arguments:'
+    print '    -r, --repository=<repository>: repository which should be updated'
+    print
+    print 'Optional Arguments:'
+    print '    --init: this means you want to initialize an new repository, existing labels will be deleted'
+    print
+
+initialize = False
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hr:", ["init","repository="])
+except getopt.GetoptError:
+    print_help()
+    sys.exit(2)
+
+repository = ''
+
+for opt, arg in opts:
+    if opt == '-h':
+        print_help()
+        sys.exit()
+    elif opt in ("-r", "--repository"):
+        repository = arg
+    elif opt == '--init':
+        initialize = True
+    else:
+        print_help()
+        sys.exit()
+
+if repository == '':
+    print_help()
+    sys.exit()
 
 config = ConfigParser.RawConfigParser()
 labels = LabelManager(config)
-serverLabels = labels.get_server_labels()
-labels.create_labels(repo, serverLabels)
+
+if initialize:
+    labels.delete_all_labels(repository)
+    serverLabels = labels.get_all_labels()
+    labels.create_labels(repository, serverLabels)
+else:
+    serverLabels = labels.get_all_labels()
+    labels.create_labels(repository, serverLabels)
--- lib/LabelManager.py
+++ lib/LabelManager.py
@@ -4,21 +4,23 @@ import ConfigParser
 
 
 class LabelManager:
+
     def __init__(self, config):
         """
         :param ConfigParser.RawConfigParser config: read config file
         """
-        self.urlServerLabels = 'https://api.github.com/repos/nextcloud/server/labels'
         self.nextcloudBaseUrl = 'https://api.github.com/repos/nextcloud/'
-        config.read('labels.cfg')
+        config.read('github.cfg')
         self.authToken = config.get('auth', 'token')
+        self.headers = {'Authorization': 'token ' + self.authToken}
 
-    def get_server_labels(self):
+    def get_all_labels(self, repo='server'):
         """
         get all labels from the Nextcloud server repository
         :return: list of all server labels
         """
-        response = requests.get(self.urlServerLabels)
+        url = self.nextcloudBaseUrl + repo + '/labels'
+        response = requests.get(url)
         data = json.loads(response.content)
         return data
 
@@ -28,17 +30,36 @@ class LabelManager:
         :param string repo:
         :param string labels:
         """
+        url = self.nextcloudBaseUrl + repo + '/labels'
+
         for label in labels:
             # skip the feature: labels because they are server specific
             if not label['name'].startswith('feature:'):
-                print 'Create label: "' + label['name'] + '" (Color: #' + label['color'] + ')'
-                url = self.nextcloudBaseUrl + repo + '/labels'
                 payload = {
                     'name': label['name'],
                     'color': label['color'],
                 }
-                # Adding empty header as parameters are being sent in payload
-                headers = {'Authorization': 'token ' + self.authToken}
-                print 'Header:', headers
-                print 'URL:', url
-                requests.post(url, data=json.dumps(payload), headers=headers)
+                checkLabel = requests.get(url + '/' + label['name'], headers=self.headers)
+                # if the label already exists we update it, otherwise we create a new one
+                if checkLabel.status_code == 200:
+                    print 'Update label', label['name']
+                    requests.patch(url + '/' + label['name'], data=json.dumps(payload), headers=self.headers)
+                else:
+                    print 'Create label: "' + label['name'] + '" (Color: #' + label['color'] + ')'
+                    requests.post(url, data=json.dumps(payload), headers=self.headers)
+
+    def delete_all_labels(self, repo):
+        """
+        delete all labels from a give repository
+        :param repo:
+        :return:
+        """
+        if repo == 'server':
+            return
+
+        url = self.nextcloudBaseUrl + repo + '/labels/'
+        labels = self.get_all_labels(repo)
+
+        for label in labels:
+            print 'Delete label: "' + label['name']
+            requests.delete(url + label['name'], headers=self.headers)
