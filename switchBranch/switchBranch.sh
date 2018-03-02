fca4c54bdaa15231cf9432611d92f503b8283869
Bjoern Schiessle <bjoern@schiessle.org>
2018-03-02 15:12:29 +0100

    improve the switchBranch script

--- switchBranch/switchBranch.sh
+++ switchBranch/switchBranch.sh
@@ -2,38 +2,62 @@
 # switch to a new branch or tag, if no parameter given we switch to master
 # script needs to be executed in the root of a Nextcloud installation
 
+branch="master"
+fast_forward=true
+
+for i in "$@"
+do
+    case $i in
+	-u|--update)
+            fast_forward=false
+	    ;;
+	-h|--help)
+	    echo ""
+	    echo "Usage: switchBranch [OPTION] [branch]"
+	    echo ""
+	    echo "Branch is optional, if nothing is specified we will switch to master"
+	    echo ""
+	    echo "-u, --update       will perform 'git fetch' and git pull' to make sure that all repositories are up-to-data (will take longer)"
+	    echo ""
+	    exit;
+	    ;;
+	*)
+            branch=$i
+	    ;;
+    esac
+done
+
 #launch ssh agent
 eval `ssh-agent -s`
 ssh-add
 
-if [ $# -eq 0 ]
-  then
-    branch=master
-else
-    branch=$1
-fi
-
 for f in apps/*; do
-    if [[ -d $f ]]; then
+    if [[ -d $f/.git ]]; then
 	echo "switch $f to $branch"
         cd $f
 	git checkout $branch &> /dev/null
-	# keep this out for now, for performance reasons
-	#exit_status=$?
-	#if [ $exit_status -eq 1 ]; then
-	#    git fetch &> /dev/null
-    	#    git checkout $branch &> /dev/null
-	#fi
+	if [ "$fast_forward" = false ]; then
+	    exit_status=$?
+	    if [ $exit_status -eq 1 ]; then
+		git fetch &> /dev/null
+		git checkout $branch &> /dev/null
+	    fi
+	    git pull &> /dev/null
+	fi
 	cd - &> /dev/null
     fi
 done
 
-echo "run git fetch..."
-git fetch &> /dev/null
+if [ "$fast_forward" = false ]; then
+    echo "run git fetch..."
+    git fetch &> /dev/null
+fi
 echo "switch server to $branch"
 git checkout $branch &> /dev/null
-echo "run git pull..."
-git pull &> /dev/null
+if [ "$fast_forward" = false ]; then
+    echo "run git pull..."
+    git pull &> /dev/null
+fi
 echo "update submodules..."
 git submodule update --init
 
